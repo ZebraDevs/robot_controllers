be67ecc89b8ce2b113bb3e8694ff9d317b675f90
Michael Ferguson <mfergs7@gmail.com>
2014-11-01 18:05:18 -0700

    add parallel gripper

---
+++ robot_controllers/include/robot_controllers/parallel_gripper.h
@@ -0,0 +1,118 @@
+/*********************************************************************
+ *  Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2014, FYS Systems Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of FYS Systems Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+// Author: Michael Ferguson
+
+#ifndef ROBOT_CONTROLLERS_PARALLEL_GRIPPER_H
+#define ROBOT_CONTROLLERS_PARALLEL_GRIPPER_H
+
+#include <string>
+#include <vector>
+
+#include <ros/ros.h>
+#include <robot_controllers_interface/controller.h>
+#include <robot_controllers_interface/joint_handle.h>
+#include <robot_controllers_interface/controller_manager.h>
+#include <control_msgs/GripperCommandAction.h>
+#include <actionlib/server/simple_action_server.h>
+
+namespace robot_controllers
+{
+
+/**
+ * @brief Controller for a parallel jaw gripper, is really only intended for
+ *        use in simulation.
+ */
+class ParallelGripperController : public Controller
+{
+  typedef actionlib::SimpleActionServer<control_msgs::GripperCommandAction> server_t;
+
+public:
+  ParallelGripperController() : initialized_(false) {}
+  virtual ~ParallelGripperController() {}
+
+  /**
+   * @brief Initialize the controller and any required data structures.
+   * @param nh Node handle for this controller.
+   * @param manager The controller manager instance, this is needed for the
+   *        controller to get information about joints, etc.
+   * @returns 0 if succesfully configured, negative values are error codes.
+   */
+  virtual int init(ros::NodeHandle& nh, ControllerManager* manager);
+
+  /**
+   * @brief Attempt to start the controller. This should be called only by the
+   *        ControllerManager instance.
+   * @returns True if successfully started, false otherwise.
+   */
+  virtual bool start();
+
+  /**
+   * @brief Attempt to stop the controller. This should be called only by the
+   *        ControllerManager instance.
+   * @param force Should we force the controller to stop? Some controllers
+   *        may wish to continue running until they absolutely have to stop.
+   * @returns True if successfully stopped, false otherwise.
+   */
+  virtual bool stop(bool force);
+
+  /**
+   * @brief This is the update loop for the controller.
+   * @param time The system time.
+   * @param dt The timestep since last call to update.
+   */
+  virtual void update(const ros::Time& now, const ros::Duration& dt);
+
+  /** @brief Get the names of joints/controllers which this controller commands. */
+  virtual std::vector<std::string> getCommandedNames();
+
+  /** @brief Get the names of joints/controllers which this controller exclusively claims. */
+  virtual std::vector<std::string> getClaimedNames();
+
+private:
+  void executeCb(const control_msgs::GripperCommandGoalConstPtr& goal);
+
+  bool initialized_;
+  ControllerManager* manager_;
+
+  JointHandlePtr left_;
+  JointHandlePtr right_;
+
+  double goal_, effort_, max_position_, max_effort_;
+  boost::shared_ptr<server_t> server_;
+};
+
+}  // namespace robot_controllers
+
+#endif  // ROBOT_CONTROLLERS_PARALLEL_GRIPPER_H
---
+++ robot_controllers/src/parallel_gripper.cpp
@@ -0,0 +1,248 @@
+/*********************************************************************
+ *  Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2014, FYS Systems Inc.
+ *  Copyright (c) 2014, Unbounded Robotics Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of FYS Systems Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+// Author: Michael Ferguson
+
+#include <pluginlib/class_list_macros.h>
+#include <robot_controllers/parallel_gripper.h>
+
+PLUGINLIB_EXPORT_CLASS(robot_controllers::ParallelGripperController, robot_controllers::Controller)
+
+namespace robot_controllers
+{
+
+int ParallelGripperController::init(ros::NodeHandle& nh, ControllerManager* manager)
+{
+  // We absolutely need access to the controller manager
+  if (!manager)
+  {
+    initialized_ = false;
+    return -1;
+  }
+
+  Controller::init(nh, manager);
+  manager_ = manager;
+
+  // Setup Joints */
+  std::string l_name, r_name;
+  nh.param<std::string>("l_gripper_joint", l_name, "l_gripper_joint");
+  nh.param<std::string>("r_gripper_joint", r_name, "r_gripper_joint");
+  left_ = manager_->getJointHandle(l_name);
+  right_ = manager_->getJointHandle(r_name);
+
+  // Start action server
+  server_.reset(new server_t(nh, "",
+                boost::bind(&ParallelGripperController::executeCb, this, _1),
+                false));
+  server_->start();
+
+  // Get Params
+  nh.param<double>("max_position", max_position_, 0.1);
+  nh.param<double>("max_effort", max_effort_, 10.0);
+
+  // Set default to max
+  goal_ = max_position_;
+  effort_ = max_effort_;
+
+  initialized_ = true;
+  return 0;
+}
+
+bool ParallelGripperController::start()
+{
+  if (!initialized_)
+  {
+    ROS_ERROR_NAMED("ParallelGripperController",
+                    "Unable to start, not initialized.");
+    return false;
+  }
+
+  if (!server_->isActive())
+  {
+    ROS_ERROR_NAMED("ParallelGripperController",
+                    "Unable to start, action server is not active.");
+    return false;
+  }
+
+  return true;
+}
+
+bool ParallelGripperController::stop(bool force)
+{
+  if (!initialized_)
+    return true;
+
+  if (server_->isActive())
+  {
+    if (force)
+    {
+      // Shut down the action
+      server_->setPreempted();
+      return true;
+    }
+
+    // Do not abort unless forced
+    return false;
+  }
+
+  // Just holding position, go ahead and stop us
+  return true;
+}
+
+void ParallelGripperController::update(const ros::Time& now, const ros::Duration& dt)
+{
+  if (!initialized_)
+    return;
+
+  left_->setPosition(goal_/2.0, 0, effort_);
+  right_->setPosition(goal_/2.0, 0, effort_);
+}
+
+void ParallelGripperController::executeCb(const control_msgs::GripperCommandGoalConstPtr& goal)
+{
+  control_msgs::GripperCommandFeedback feedback;
+  control_msgs::GripperCommandResult result;
+
+  if (!initialized_)
+  {
+    server_->setAborted(result, "Controller is not initialized.");
+    return;
+  }
+
+  if (manager_->requestStart(getName()) != 0)
+  {
+    server_->setAborted(result, "Cannot execute, unable to start controller.");
+    ROS_ERROR_NAMED("ParallelGripperController",
+                    "Cannot execute, unable to start controller.");
+    return;
+  }
+
+  // Effort of 0.0 means use max
+  if (goal->command.max_effort <= 0.0 || goal->command.max_effort > max_effort_)
+  {
+    effort_ = max_effort_;
+  }
+  else
+  {
+    effort_ = goal->command.max_effort;
+  }
+
+  // Set goal position
+  if (goal->command.position > max_position_)
+  {
+    goal_ = max_position_;
+  }
+  else if (goal->command.position < 0.0)
+  {
+    goal_ = 0.0;
+  }
+  else
+  {
+    goal_ = goal->command.position;
+  }
+
+  // Track position/time for stall detection
+  float last_position = left_->getPosition() + right_->getPosition();
+  ros::Time last_position_time = ros::Time::now();
+
+  ros::Rate r(50);
+  while (true)
+  {
+    // Abort detection
+    if (server_->isPreemptRequested() || !ros::ok())
+    {
+      ROS_DEBUG_NAMED("ParallelGripperController", "Command preempted.");
+      server_->setPreempted();
+      break;
+    }
+
+    // Publish feedback before possibly completing
+    feedback.position = left_->getPosition() + right_->getPosition();
+    feedback.effort = left_->getEffort() + right_->getEffort();
+    feedback.reached_goal = false;
+    feedback.stalled = false;
+    server_->publishFeedback(feedback);
+
+    // Goal detection
+    if (fabs(feedback.position - goal->command.position) < 0.002)
+    {
+      result.position = feedback.position;
+      result.effort = feedback.effort;
+      result.reached_goal = true;
+      result.stalled = false;
+      ROS_DEBUG_NAMED("ParallelGripperController", "Command Succeeded.");
+      server_->setSucceeded(result);
+      return;
+    }
+
+    // Stall detection
+    if (fabs(feedback.position - last_position) > 0.005)
+    {
+      last_position = feedback.position;
+      last_position_time = ros::Time::now();
+    }
+    else
+    {
+      if (ros::Time::now() - last_position_time > ros::Duration(2.0))
+      {
+        result.position = feedback.position;
+        result.effort = feedback.effort;
+        result.reached_goal = false;
+        result.stalled = true;
+        ROS_DEBUG_NAMED("ParallelGripperController", "Gripper stalled, but succeeding.");
+        server_->setSucceeded(result);
+        return;
+      }
+    }
+
+    r.sleep();
+  }
+}
+
+std::vector<std::string> ParallelGripperController::getCommandedNames()
+{
+  std::vector<std::string> names;
+  names.push_back(left_->getName());
+  names.push_back(right_->getName());
+  return names;
+}
+
+std::vector<std::string> ParallelGripperController::getClaimedNames()
+{
+  // Claimed == commanded.
+  return getCommandedNames();
+}
+
+}  // namespace robot_controllers
